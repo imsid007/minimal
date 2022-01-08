2bc92500f8be32481870dd51b0296b9a20ebd746
imsid1996 <siddharth@mybytecode.com>
2022-01-08 20:32:08 +0530

    tabs

--- src/sections/@dashboard/user/profile/index.ts
+++ src/sections/@dashboard/user/profile/index.ts
@@ -1,3 +1,4 @@
+export {default as About} from './About';
 export { default as Profile } from './Profile';
 export { default as ProfileCover } from './ProfileCover';
 export { default as ProfileAbout } from './ProfileAbout';
